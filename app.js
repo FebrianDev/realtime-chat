2c42ce16bca6f95e650f9b8f598741bb2a85561a
Febrian <59677015+FebrianDev@users.noreply.github.com>
2021-06-08 22:49:01 +0700

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const server = http.createServer(app)
 const {Server} = require('socket.io')
 const io = new Server(server)
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}));
